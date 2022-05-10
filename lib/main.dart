9b9217c9c3938661637d29063842aa829f9930a2
vaca22 <wsmslgh@gmail.com>
2022-05-10 21:10:03 +0800

    dsfgfedsgdfsgfdg

--- lib/main.dart
+++ lib/main.dart
@@ -58,6 +58,19 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
       title: 'fuckyou',
       home: Scaffold(
@@ -65,7 +78,13 @@ class MyApp extends StatelessWidget {
             title: const Text('fuckyou'),
           ),
           body: Column(
-            children: [titleSection, buttonSection],
+            children: [
+              Image.asset('images/lake.jpg',
+                  width: 600, height: 240, fit: BoxFit.cover),
+              titleSection,
+              buttonSection,
+              textSection
+            ],
           )),
     );
   }
